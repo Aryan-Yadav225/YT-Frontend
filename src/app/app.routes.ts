5256224782eaaf7e007e53cb9e8c4380740b8f40
Aryan Yadav <aryan225@code@gmail.com>
2025-08-01 19:59:16 +0530

    Added Angular Material, Ngx-File-Drop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'upload', pathMatch: 'full' },
+  {
+    path: 'upload',
+    loadComponent: () =>
+      import('./upload-video/upload-video.component').then(
+        (m) => m.UploadVideoComponent
+      ),
+  },
+];
