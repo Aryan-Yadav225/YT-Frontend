4ffadb602253d2f8a447ac76044a7d7f736275f5
Aryan Yadav <aryan225@code@gmail.com>
2025-08-03 16:21:59 +0530

    video in working condition

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,8 +1,20 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  NgZone,
+  AfterViewChecked,
+  SimpleChanges,
+  OnChanges,
+} from '@angular/core';
 import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play';
 import { VgCoreModule } from '@videogular/ngx-videogular/core';
 import { VgBufferingModule } from '@videogular/ngx-videogular/buffering';
 import { VgControlsModule } from '@videogular/ngx-videogular/controls';
+
+import { Router, RouteConfigLoadEnd } from '@angular/router';
 
 @Component({
   selector: 'app-video-player',
@@ -15,6 +27,17 @@ import { VgControlsModule } from '@videogular/ngx-videogular/controls';
   templateUrl: './video-player.component.html',
   styleUrl: './video-player.component.css',
 })
-export class VideoPlayerComponent {
+export class VideoPlayerComponent implements OnChanges {
   @Input() videoURL: string = '';
+  videoReady = false;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['videoURL'] && this.videoURL) {
+      // Reset player to force full DOM re-init
+      this.videoReady = false;
+      setTimeout(() => {
+        this.videoReady = true;
+      });
+    }
+  }
 }
