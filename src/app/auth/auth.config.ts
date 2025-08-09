0193b49bdd853a2e980d93843ef67ece4c2580bb
Aryan Yadav <aryan225@code@gmail.com>
2025-08-09 14:42:20 +0530

    initial Auth commit

---
+++ src/app/auth/auth.config.ts
@@ -0,0 +1,13 @@
+import { PassedInitialConfig } from 'angular-auth-oidc-client';
+
+export const authConfig: PassedInitialConfig = {
+  config: {
+            authority: 'dev-ihlldbpqyyl8j7wp.us.auth0.com',
+            redirectUrl: window.location.origin,
+            clientId: 'hgjQEv6PQnEcYZAGy7NJrGwqbJCLwcYm',
+            scope: 'openid profile offline_access',
+            responseType: 'code',
+            silentRenew: true,
+            useRefreshToken: true,
+        }
+}
